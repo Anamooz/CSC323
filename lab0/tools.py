5ff7179f24455f623f0100adf0fb5d6353fba889
Brian Kwong <bkwong01@calpoly.edu>
2025-01-09 10:27:38 -0800

    Conversion tools updated

--- lab0/tools.py
+++ lab0/tools.py
@@ -1,20 +1,21 @@
 import base64
 
 
+# Converts an ASCII string to hex encoding
 def ascii_to_hex(asciiText: str) -> str:
     return asciiText.encode().hex()
 
 
+# Converts an hex encoded string to ASCII text
 def hex_to_ascii(hex: str) -> str:
     return bytes.fromhex(hex).decode()
 
 
+# Converts an ASCII string to base64 encoding
 def ascii_to_base64(asciiText: str) -> str:
     return base64.b64encode(asciiText.encode()).decode()
 
 
-# def ascii_to_base64(base64Bytes: bytes) -> str:
-#     return base64.decode(base64Bytes)
-
-
-print(ascii_to_base64("Hello, World!"))
+# Converts base64 encoded string to ASCII text
+def base64_to_ascii(base64Bytes: str) -> str:
+    return base64.b64decode(base64Bytes.encode()).decode()
