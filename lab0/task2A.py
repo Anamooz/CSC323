a20fca0c7ae005f40fdb45596c3ef68099abb92b
Anamooz <pthynktwice@gmail.com>
2025-01-09 11:22:03 -0800

    task2a

--- lab0/task2A.py
+++ lab0/task2A.py
@@ -1,2 +1,7 @@
 def implementXOR(inputString: bytes, key: bytes) -> bytes:
+    if len(inputString) > len(key):
+        number = (len(inputString) // len(key) + 1)
+        key *= number
     return bytes([a ^ b for a, b in zip(inputString, key)])
+
+print(implementXOR("ABCDEF".encode(), "AB".encode()))
