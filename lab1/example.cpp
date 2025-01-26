910d402e3c0fbdc9d8fca55f6ace648d92e8251f
Brian Kwong <bkwong01@calpoly.edu>
2025-01-26 13:44:25 -0800

    Updated task1

---
+++ lab1/example.cpp
@@ -0,0 +1,21 @@
+#include <random>
+#include<iostream>
+#include <array>
+
+int main(int argc, char* argv[])
+{
+    using namespace std;
+
+    if(argc < 2){
+        cout << "Please provide a seed" << endl;
+        return 1;
+    }
+
+    unsigned int seed = std::stoi(argv[1]);
+    array<unsigned int,10> number;
+    mt19937 gen(seed);
+    for(int i = 0; i < 10; i++){
+        number[i] = gen();
+        cout << number[i] << endl;
+    }
+}
--- lab1/task1.py
+++ lab1/task1.py
@@ -28,6 +28,7 @@ class MT19937:
         self.f = 1812433253
         self.lower_mask = (1 << self.r) - 1  # All 31 lower bits are set
         self.upper_mask = 1 << self.r  # Only the 32nd bit is set
+        self.maxUInt32 = 2**32 - 1  # Max value for a 32 bit unsigned integer 0xFFFFFFFF
 
         # Initialize state array
 
@@ -49,45 +50,131 @@ class MT19937:
 
         """
         self.mt[0] = seed  # Sets the seed as the first element in the buffer
+
+        # For each of the remaining elements in the buffer calculate that number using the following formula :
+
+        # x[i] = f * ((Xi-1) XOR ((Xi-1 >> 30 )) + i
+
         for i in range(1, self.n):
             self.mt[i] = (
                 self.f * (self.mt[i - 1] ^ (self.mt[i - 1] >> (self.w - 2))) + i
-            )
-        self.index = self.n
+            ) & self.maxUInt32  # Ensures we only use lower 32 bits Mimics the C code
+        self.index = (
+            self.n
+        )  # Note this inital buffer is not used directly to generate numbers
 
     def twist(self):
-        """Generate the next n values from the series x."""
+        """
+
+        Generate the next set of n values in the buffer once the previous set of numbers have been exhausted.
+
+        ((Xk + n ) where k > 0
+                    ((Xk + n) =  ((Xk + m) (CONCAT(upperBits((Xk),lowerBits((Xk + 1) )A)
+        Which translates to the following steps:
+            1. x[i] = (x[i] & upper_mask) + (x[i + 1] & lower_mask)
+            2. If x[i] is odd then xA = x[i] >> 1 XOR a else just xA = x[i] >> 1) # Applies the matrix A transformation
+            3. x[i] = x[k + m] XOR xA
+
+        """
+
         for i in range(self.n):
-            x = (self.mt[i] & self.upper_mask) + (
-                self.mt[(i + 1) % self.n] & self.lower_mask
-            )
+
+            first_element = self.mt[i]
+            second_element = self.mt[
+                (i + 1) % self.n
+            ]  # Checks if the element if over the size of the size of the buffer
+            # If so wrap around to the beginning of the buffer
+
+            x = (first_element & self.upper_mask) + (second_element & self.lower_mask)
+
+            # Apply the matrix A transformation to the value of x using the following formula :
+            # If x[i] is odd then xA = x[i] >> 1 XOR a else just xA = x[i] >> 1)
+            # Even is defined as the last bit not being set
+
             xA = x >> 1
-            if x % 2 != 0:  # lowest bit of x is 1
+            if x % 2:  # lowest bit of x is 1
                 xA ^= self.a
-            self.mt[i] = self.mt[(i + self.m) % self.n] ^ xA
-        self.index = 0
+
+            offset_element = self.mt[(i + self.m) % self.n]
+            # XOR xA with X[k+m] element to get the final twisted value
+            self.mt[i] = offset_element ^ xA
+        self.index = 0  # Reset the index back to 0
 
     def extract_number(self):
-        """Extract a tempered value based on mt[index]."""
+        """
+
+        The following function that pops a number off the buffer and applies the tempering function to it.
+
+        The tempering function is defined as follows:
+            1. y = x XOR ((x >> u) AND d)
+            2. y = y XOR ((y << s) AND b)
+            3. y = y XOR ((y << t) AND c)
+            4. y = y XOR (y >> 1)
+
+        The tempered value is then returned.
+
+        """
+
+        # If the buffer has been exhausted generate a new set of numbers before popping the next number
         if self.index >= self.n:
-            if self.index > self.n:
-                raise Exception("Generator was never seeded")
             self.twist()
 
+        # Pop the next number from the buffer
         y = self.mt[self.index]
+        # Slide the index to the next number for the next round of generation
         self.index += 1
-
         # Applies the tempering function
         y ^= (y >> self.u) & self.d
         y ^= (y << self.s) & self.b
         y ^= (y << self.t) & self.c
         y ^= y >> self.l
 
-        return y & 0xFFFFFFFF
+        return y  # Returns the tempered value
 
 
 # Example usage and verification
 class Test_MT19937(unittest.TestCase):
+
+    def test_correctness(self):
+        """
+        Test if the function returns the correct sequence of numbers
+        This verification is deterministic to the spec of the algorithm given a specific seed.
+
+        The output is verified against the output of the standard C++11 implementation of MT19937
+        See `example.cpp` fo more details.
+
+        `❯ ./numbers.out 88888
+        2907235558
+        2208549397
+        2766912144
+        3328673921
+        1295862538
+        784666349
+        361922383
+        3765264219
+        3750128615
+        1881693225`
+
+        """
+
+        expected = [
+            2907235558,
+            2208549397,
+            2766912144,
+            3328673921,
+            1295862538,
+            784666349,
+            361922383,
+            3765264219,
+            3750128615,
+            1881693225,
+        ]
+
+        seed = 88888
+        mt = MT19937(seed)
+        # Generate the first 10 numbers
+        results = [mt.extract_number() for _ in range(10)]
+        self.assertEqual(results, expected)
 
     def test_deterministic_output(self):
         """
@@ -132,16 +219,17 @@ class Test_MT19937(unittest.TestCase):
         seed = 88888
         mt = MT19937(seed)
         # Generate the first 10 numbers
-        results = set([mt.extract_number() for _ in range(10)])
+        results = [mt.extract_number() for _ in range(10)]
 
         # Generate the first 10 numbers with a different seed
         mt2 = MT19937(seed + 1)
-        results2 = set([mt2.extract_number() for _ in range(10)])
+        results2 = [mt2.extract_number() for _ in range(10)]
 
         # There shoiuld be no intersection between the two sets of numbers generated
-        self.assertEqual(len(results.intersection(results2)), 0)
+        for number in enumerate(results):
+            self.assertNotEqual(number[1], results2[number[0]])
 
-    def invalid_seeds(self):
+    def test_invalid_seeds(self):
         """
 
         Tests if invalid seeds are handled correctly and raises a ValueError
@@ -156,7 +244,3 @@ class Test_MT19937(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-    seed = 88888
-    mt = MT19937(seed)
-    # Generate the first 10 numbers
-    results = set([mt.extract_number() for _ in range(10)])
