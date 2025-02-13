2e003114150ec0c5e602e82b46a6955dc373079f
Brian Kwong <bkwong01@calpoly.edu>
2025-02-13 09:36:06 -0800

    Save

---
+++ lab2/task3a.py
@@ -0,0 +1,40 @@
+from pad import *
+from tools import *
+from constants import *
+from Crypto.Cipher import AES
+
+
+def cbc_encrypt(plaintext: bytes, key: bytes, iv: bytes) -> bytes:
+    cipher = AES.new(key, AES.MODE_ECB)
+    plaintext = pad(plaintext)
+    blocks = [plaintext[i : i + 16] for i in range(0, len(plaintext), 16)]
+    ciphertext = iv
+    for block in blocks:
+        block = implementXOR(block, iv)
+        iv = cipher.encrypt(block)
+        ciphertext += iv
+    return ciphertext
+
+
+def cbc_decrypt(ciphertext: bytes, key: bytes) -> bytes:
+    cipher = AES.new(key, AES.MODE_ECB)
+    iv = ciphertext[:16]
+    ciphertext = ciphertext[16:]
+    blocks = [ciphertext[i : i + 16] for i in range(0, len(ciphertext), 16)]
+    plaintext = b""
+    for block in blocks:
+        decrypted = cipher.decrypt(block)
+        plaintext += implementXOR(decrypted, iv)
+        iv = block
+    plaintext = unpad(plaintext)
+    return plaintext
+
+
+KEY = "MIND ON MY MONEY"
+IV = "MONEY ON MY MIND"
+with open("Lab2.TaskIII.A.txt", "r") as f:
+    cipertext = f.read()
+    cipertext = base64_to_ascii(cipertext)
+    ciphertext = IV.encode() + cipertext
+    plaintext = cbc_decrypt(cipertext, KEY.encode())
+    print(plaintext.decode())
--- lab2/task3b.py
+++ lab2/task3b.py
@@ -3,44 +3,43 @@ from tools import *
 from selenium import webdriver
 
 
+def cookieMaker(cookie: bytes):
+    newBlock = b""
+    MSG_BLOCK = b"&role=user" + (b"\x00" * 5) + bytes.fromhex("06")
+    BLOCK_DESIRED = b"&role=admin" + (b"\x00" * 4) + bytes.fromhex("05")
+    iv = cookie[:16]
+    block2 = cookie[16:32]
+    for i in range(16):
+        newBlock += int.to_bytes(block2[i] ^ MSG_BLOCK[i] ^ BLOCK_DESIRED[i])
+    return iv + block2 + newBlock + cookie[32:]
+
+
 # Set the URL for the session
 
 # Make 10000 random accounts
 URL = "http://localhost:8080/register"
-# for i in range(10000):
-#     payload = {"user": f"user{i}", "password": "password123", "register": ""}
-#     requests.post("http://localhost:8080/register", data=payload)
-#     if i % 1000 == 0:
-#         print(f"Created {i} accounts")
 
 # # make the attack account
 session = requests.Session()
-username = ("x" * 11) + "admin" + (chr(0) * 10) + chr(11) + ("x" * 4)
+username = "x" * 5
 payload = {"user": username, "password": "password123", "register": ""}
 response = session.post(URL, data=payload)
 
-
-# user=xxxxxx...|admin....|xxxx&uid=1&role=user.....
-# user=xxxxxx...admin....&uid=1&role=admin....
 LOGIN_URL = "http://localhost:8080/"
 payload = {"user": username, "password": "password123", "Login": ""}
 response = session.post(LOGIN_URL, data=payload)
 
-
 cookie = session.cookies.get_dict()["auth_token"]
 cookie = hex_to_ascii(cookie)
-
-adminCookie = cookie[16:32]
-newAdminCookie = cookie[:-16] + adminCookie
-newAdminCookie = ascii_to_hex(newAdminCookie)
-session.cookies.set("auth_token", newAdminCookie)
+new_cookie = cookieMaker(cookie)
+session.cookies.set("auth_token", ascii_to_hex(new_cookie))
 
 HOME_URL = "http://localhost:8080/home"
 response = session.get(HOME_URL)
 with open("output.html", "w") as f:
     f.write(response.text)
-driver = webdriver.Edge()
+driver = webdriver.Chrome()
 driver.get(LOGIN_URL)
-driver.add_cookie({"name": "auth_token", "value": newAdminCookie})
+driver.add_cookie({"name": "auth_token", "value": ascii_to_hex(new_cookie)})
 driver.get(HOME_URL)
 input("Press Enter to continue...")
